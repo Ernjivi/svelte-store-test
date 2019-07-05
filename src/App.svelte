d8b32eede67e6169687c761832d83f1294110e14
Ernesto Jiménez <ernesto@bedu.org>
2019-07-05 12:40:53 -0500

    Writable stores

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+  <script>
+    import { count } from './store.js';
+    import Increment from './Increment.svelte';
+    import Decrement from './Decrement.svelte';
+    import Resetter from './Resetter.svelte';
+
+    let count_value;
+
+    const unsubscribe = count.subscribe(value => {
+      count_value = value;
+    })
+  </script>
+
+  <style>
+    h1 {
+      color: purple;
+    }
+  </style>
+
+  <h1>The count is {count_value}</h1>
+  <Increment />
+  <Decrement />
+  <Resetter />
+
